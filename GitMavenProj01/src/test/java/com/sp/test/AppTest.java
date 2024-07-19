ee93006459eb184000901ab3f0553550b9674427
DELL <DELL@SP-Sugyan-swain>
2024-07-19 14:11:29 +0530

    First TL Commit

--- GitMavenProj01/src/test/java/com/sp/test/AppTest.java
+++ GitMavenProj01/src/test/java/com/sp/test/AppTest.java
@@ -9,6 +9,7 @@ import com.sp.service.Arithmetic;
 public class AppTest {
 	
 	
+	
 	@Test
 	public void TestWithPositive() {
 		Arithmetic ar = new Arithmetic();
@@ -17,6 +18,21 @@ public class AppTest {
 		assertEquals(expected, actual);
 		
 	}
-	
+	@Test
+	public void TestWithNegetive() {
+		Arithmetic ar = new Arithmetic();
+		int expected = -300 ;
+		int actual 	 = ar.sum(-100, -200);
+		assertEquals(expected, actual);
+		
+	}
+	@Test
+	public void TestWithZero() {
+		Arithmetic ar = new Arithmetic();
+		int expected = -0 ;
+		int actual 	 = ar.sum(0, 0);
+		assertEquals(expected, actual);
+		
+	}
 	
 }
