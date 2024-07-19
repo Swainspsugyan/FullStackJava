b632e7668ff7e9b401aef17cfa85696c355ed596
DELL <DELL@SP-Sugyan-swain>
2024-07-19 13:55:44 +0530

    Dev1 - Arithmetic Operation

--- GitMavenProj01/src/main/java/com/sp/main/ApplicationRun.java
+++ GitMavenProj01/src/main/java/com/sp/main/ApplicationRun.java
@@ -1,10 +1,19 @@
 package com.sp.main;
 
+import com.sp.service.Arithmetic;
+
 public class ApplicationRun {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Arithmetic ar = new Arithmetic();
+		System.out.println("Sum is : " + ar.sum(100, 200));
+		
+		
+		
+		
+		
 	}
 
 }
--- GitMavenProj01/src/test/java/com/sp/test/AppTest.java
+++ GitMavenProj01/src/test/java/com/sp/test/AppTest.java
@@ -1,5 +1,22 @@
 package com.sp.test;
 
-public class AppTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import com.sp.service.Arithmetic;
+
+public class AppTest {
+	
+	
+	@Test
+	public void TestWithPositive() {
+		Arithmetic ar = new Arithmetic();
+		int expected = 300 ;
+		int actual 	 = ar.sum(100, 200);
+		assertEquals(expected, actual);
+		
+	}
+	
+	
 }
